461e1f3be9401e2a8d6f351d8f60ba29c5c3833f
azeez1776 <aboud3am81@gmail.com>
2022-01-03 01:19:04 +0300

    added the item Schema

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -1,1 +1,16 @@
 import mongoose from 'mongoose';
+
+import {Schema} from "mongoose";
+
+const item = Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:50
+    },
+   notes:String,
+    due:Date
+},
+    {timestamps: true}
+)
