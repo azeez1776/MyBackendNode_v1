c2102d4d4119c302a81a631e27abe0ddfc412941
azeez1776 <aboud3am81@gmail.com>
2022-01-02 21:42:40 +0300

    tested the port 3000

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const app = express();
 
 const PORT = 3000;
 
+app.get('/', (req, res) => {
+    res.send("Hello World")
+})
+
 
 app.listen(PORT, () =>{
     console.log(`Listening to port ${PORT}`)
