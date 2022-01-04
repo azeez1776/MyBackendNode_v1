7f46be5ba9d7d79bd882da754c2d0ebe83b2ccdf
azeez1776 <aboud3am81@gmail.com>
2022-01-04 20:45:03 +0300

    created the create item controller

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -12,13 +12,23 @@ export const getOne = model => async (req, res) => {
 
 export const getAll = model => async (req, res) => {
     try{
-        let doc = await model.find({})
+        let doc = await model.find()
             .lean()
             .exec()
         res.status(200).json(doc).end()
     } catch(e){
         console.log(e);
         res.status(401).json({message: 'Failed to retrieve from the Database'}).end();
+    }
+}
+
+export const create = model => async (req, res) => {
+    try{
+        let doc = await model.create({...req.body})
+        res.status(200).json(doc).end()
+    }catch (e) {
+        console.log(e);
+        res.status(400).json({message:'Failed to create, something wrong with the Database'})
     }
 }
 
