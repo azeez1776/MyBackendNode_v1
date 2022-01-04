1cfe7035ccafd58cb6e8a001dced9e587420468b
azeez1776 <aboud3am81@gmail.com>
2022-01-04 19:48:23 +0300

    included the items from the database, and made an error message just incase an error occures

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -3,22 +3,22 @@ export const getOne = model => async (req, res) => {
         let doc = await model.findOne({_id:req.params.id})
             .lean()
             .exec()
-        res.status(200).end()
+        res.status(200).json(doc).end()
     } catch(e){
         console.log(e)
-        res.status(401).end()
+        res.status(401).json({message:"Failed to retrieve from the Database"}).end()
     }
 }
 
 export const getAll = model => async (req, res) => {
     try{
         let doc = await model.find({})
-
+            .lean()
             .exec()
-        res.status(200).end()
+        res.status(200).json(doc).end()
     } catch(e){
         console.log(e);
-        res.status(401).end();
+        res.status(401).json({message: 'Failed to retrieve from the Database'}).end();
     }
 }
 
