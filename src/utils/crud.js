cde78918771b0b3e445424e75e3799be13595bc0
azeez1776 <aboud3am81@gmail.com>
2022-01-04 18:58:27 +0300

    added the getAll function on the crud.js file

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -9,3 +9,15 @@ export const getOne = model => async (req, res) => {
         res.status(401).end()
     }
 }
+
+export const getAll = model => async (req, res) => {
+    try{
+        let doc = await model.find({})
+            .lean()
+            .exec()
+        res.status(200).end()
+    } catch(e){
+        console.log(e);
+        res.status(401).end();
+    }
+}
