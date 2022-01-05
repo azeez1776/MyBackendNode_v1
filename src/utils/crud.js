22577388298b7b8df5a457fcc94b650073690534
azeez1776 <aboud3am81@gmail.com>
2022-01-06 00:56:16 +0300

    added the update controller

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -42,9 +42,20 @@ export const remove = model => async (req, res) => {
     }
 }
 
+export const update = model => async (req, res) =>{
+    try{
+        const doc = await model.findOneAndUpdate({_id:req.params.id})
+        res.status(200).json(doc).end()
+    }catch (e) {
+        console.log(e)
+        res.status(401).json({message:'Failed to update, something wrong with the Database'}).end()
+    }
+}
+
 export const crudController = model => ({
     getOne:getOne(model),
     getAll:getAll(model),
     create:create(model),
-    remove:remove(model)
+    remove:remove(model),
+    update:update(model)
 })
