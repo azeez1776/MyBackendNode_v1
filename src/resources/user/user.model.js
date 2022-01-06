340a49976e9f138bf2d2a5b458838134e73b818b
azeez1776 <aboud3am81@gmail.com>
2022-01-06 15:31:08 +0300

    made a user model

---
+++ src/resources/user/user.model.js
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true
+    },
+    password:{
+        type:String,
+        required: true,
+        minLength:8
+    },
+
+},
+    {timestamp:true}
+)
