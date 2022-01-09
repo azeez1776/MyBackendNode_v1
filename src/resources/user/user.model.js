1cf25621b6583638395722661dc928907881fdba
azeez1776 <aboud3am81@gmail.com>
2022-01-09 17:23:44 +0300

    added a functionality to compare password with the help of bcrypt

--- src/resources/user/user.model.js
+++ src/resources/user/user.model.js
@@ -1,18 +1,43 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-        trim:true
-    },
-    password:{
-        type:String,
+    email: {
+        type: String,
         required: true,
-        minLength:8
+        unique: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 8
     },
 
 },
-    {timestamps:true}
+    { timestamps: true }
+);
+
+userSchema.pre('save', function (next) {
+    if (!this.isModified('password'))
+        next()
+    bcrypt.hash(this.password, 8, (err, hash) => {
+        if (err) {
+            next(err)
+        }
+
+        this.password = hash;
+        next()
+    })
+
+    userSchema.methods.checkPassword = function (password) {
+        const paswordHash = this.password;
+        return new Promise((resolve, reject) => {
+            bcrypt.compare(password, paswordHash, (err, same) => {
+                if (err) reject(err);
+                resolve(same)
+            })
+        })
+    }
+}
+
 )
