fb4712fd91bb4d6397fb0d22cca6639c9ed2093e
azeez1776 <aboud3am81@gmail.com>
2022-01-13 23:50:38 +0300

    introduced a null parameter

--- src/utils/crud.js
+++ src/utils/crud.js
@@ -1,63 +1,63 @@
 export const getOne = model => async (req, res) => {
-    try{
-        let doc = await model.findOne({_id:req.params.id})
+    try {
+        let doc = await model.findOne({ _id: req.params.id })
             .lean()
             .exec()
         res.status(200).json(doc).end()
-    } catch(e){
+    } catch (e) {
         console.log(e)
-        res.status(401).json({message:"Failed to retrieve from the Database"}).end()
+        res.status(401).json({ message: "Failed to retrieve from the Database" }).end()
     }
 }
 
 export const getAll = model => async (req, res) => {
-    try{
+    try {
         let doc = await model.find()
             .lean()
             .exec()
         res.status(200).json(doc).end()
-    } catch(e){
+    } catch (e) {
         console.log(e);
-        res.status(401).json({message: 'Failed to retrieve from the Database'}).end();
+        res.status(401).json({ message: 'Failed to retrieve from the Database' }).end();
     }
 }
 
-export const create = model => async (req, res) => {
-    try{
-        let doc = await model.create({...req.body})
+export const create = (model, user = null) => async (req, res) => {
+    try {
+        let doc = await model.create({ ...req.body })
         res.status(200).json(doc).end()
-    }catch (e) {
+    } catch (e) {
         console.log(e);
-        res.status(400).json({message:'Failed to create, something wrong with the Database'}).end()
+        res.status(400).json({ message: 'Failed to create, something wrong with the Database' }).end()
     }
 }
 
 export const remove = model => async (req, res) => {
     try {
-        let doc = await model.findOneAndRemove({_id:req.params.id})
+        let doc = await model.findOneAndRemove({ _id: req.params.id })
         res.status(200).json(doc).end()
     } catch (e) {
         console.log(e)
-        res.status(401).json({message:'Failed to delete, something wrong with the Database'}).end()
+        res.status(401).json({ message: 'Failed to delete, something wrong with the Database' }).end()
     }
 }
 
-export const update = model => async (req, res) =>{
-    try{
-        const doc = await model.findOneAndUpdate({_id:req.params.id}, req.body, {new:true})
+export const update = model => async (req, res) => {
+    try {
+        const doc = await model.findOneAndUpdate({ _id: req.params.id }, req.body, { new: true })
             .lean()
             .exec()
         res.status(200).json(doc).end()
-    }catch (e) {
+    } catch (e) {
         console.log(e)
-        res.status(401).json({message:'Failed to update, something wrong with the Database'}).end()
+        res.status(401).json({ message: 'Failed to update, something wrong with the Database' }).end()
     }
 }
 
 export const crudController = model => ({
-    getOne:getOne(model),
-    getAll:getAll(model),
-    create:create(model),
-    remove:remove(model),
-    update:update(model)
+    getOne: getOne(model),
+    getAll: getAll(model),
+    create: create(model),
+    remove: remove(model),
+    update: update(model)
 })
