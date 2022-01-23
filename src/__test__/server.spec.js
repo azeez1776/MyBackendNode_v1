577cb1516b193d5a04adb6687e3ed3d90d52a003
azeez1776 <aboud3am81@gmail.com>
2022-01-23 23:12:00 +0300

    using beforeEach in testing

--- src/__test__/server.spec.js
+++ src/__test__/server.spec.js
@@ -1,6 +1,14 @@
 import { exportAllDeclaration, EXPORTDECLARATION_TYPES } from '@babel/types';
 import request from 'supertest';
 import { app } from '../server';
+import { User } from '../resources/user/user.model';
+
+describe('API Authentication', () => {
+    let token;
+    beforeEach(async () => {
+        const user = await 
+    })
+})
 
 
 
