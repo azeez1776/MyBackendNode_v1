9a1a275810c2c46ab1f39008394aa4b2bb02248c
azeez1776 <aboud3am81@gmail.com>
2022-02-11 00:47:27 +0300

    testing for user email

--- src/resources/user/__test__/user.model.spec.js
+++ src/resources/user/__test__/user.model.spec.js
@@ -1,3 +1,15 @@
 import {User} from '../user.model.js';
 import mongoose from 'mongoose';
 
+describe("Testing the user model", () => {
+    test("Testing the user email", () => {
+        const email = User.schema.obj.email;
+        expect(email).toEqual({
+            type: String,
+            required: true,
+            unique: true,
+            trim: true
+        })
+    });
+
+})
