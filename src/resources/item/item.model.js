e750572868d94cf5b5f11a51a6414de1e864385a
azeez1776 <aboud3am81@gmail.com>
2022-02-12 23:46:16 +0300

    made the item name to be unique for a list by compound indexing I guess

--- src/resources/item/item.model.js
+++ src/resources/item/item.model.js
@@ -23,6 +23,8 @@ const itemSchema = new mongoose.Schema({
     }
 },
     { timestamps: true }
-)
+);
+
+itemSchema.index({list:1, name:1}, {unique:true});
 
 export const Item = mongoose.model('item', itemSchema);
