f9cf3e6485e232a1d331dfcd20ebf8743b0cea25
azeez1776 <aboud3am81@gmail.com>
2022-02-12 19:33:19 +0300

    added createdBy to the List model

--- src/resources/list/list.model.js
+++ src/resources/list/list.model.js
@@ -1,10 +1,16 @@
 import mongoose from 'mongoose';
 
-const ListSchema = mongoose.Schema({
+const ListSchema = new mongoose.Schema({
     name:{
         type:String,
         required:true,
         trim:true,
         maxLength:50
+    },
+    description:String,
+    createdBy:{
+        type:mongoose.SchemaTypes.ObjectId,
+        ref:'user',
+        required:true
     }
 })
