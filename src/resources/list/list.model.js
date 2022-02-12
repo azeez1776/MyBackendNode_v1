9cce9b4593a070f79ce6e970e74b53b072413fa8
azeez1776 <aboud3am81@gmail.com>
2022-02-12 19:26:55 +0300

    added a name for the list model

--- src/resources/list/list.model.js
+++ src/resources/list/list.model.js
@@ -1,1 +1,10 @@
 import mongoose from 'mongoose';
+
+const ListSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:50
+    }
+})
