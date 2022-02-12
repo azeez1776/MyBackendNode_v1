d0bd7e469ccd9ba91ed4cbc7cbcb12dd2e3829d7
azeez1776 <aboud3am81@gmail.com>
2022-02-12 20:00:33 +0300

    added route to a id distinct path, and made get, put and delete request to them

--- src/resources/list/list.router.js
+++ src/resources/list/list.router.js
@@ -6,3 +6,8 @@ const router = express.Router();
 router.route('/')
 .get(controllers.getAll)
 .post(controllers.create)
+
+router.route(':id')
+.get(controllers.getOne)
+.put(controllers.update)
+.delete(controllers.remove)
