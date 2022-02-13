8b7193b34abff00d4c162a99ff87dbba33b51c8c
azeez1776 <aboud3am81@gmail.com>
2022-02-13 10:52:44 +0300

    added test for description

--- src/resources/list/__test__/list.model.spec.js
+++ src/resources/list/__test__/list.model.spec.js
@@ -3,7 +3,20 @@ import mongoose from 'mongoose';
 
 describe("Testing List Schema", () => {
     test("testing list name", () => {
-        const name = mongoose.SchemaTypes.ObjectId;
-
+        const name = List.schema.obj.name;
+        expect(name).toEqual({
+            name:{
+                type:String,
+                required:true,
+                maxLength:50,
+                trim:true
+            }
+        })
     })
+
+    test("testing list description", () => {
+        const description = List.schema.obj.description;
+        expect(description).toEqual(String);
+    }
+
 })
