15c488708862d70377b7e267409148b931e8f8e8
azeez1776 <aboud3am81@gmail.com>
2022-02-14 21:25:47 +0300

    declared a const crud array

--- src/resources/list/__test__/list.controller.spec.js
+++ src/resources/list/__test__/list.controller.spec.js
@@ -1,6 +1,15 @@
 import {isFunction} from "lodash";
 import {crudController} from "../../../utils/crud.js";
 
-describe("Testing the crud functionality of list", ()=> {
+describe("Testing the list controller", ()=> {
+    test("testing the crud functionality",()=>{
+        const cruds = [
+            'getOne',
+            'getAll',
+            'update',
+            'remove',
+            'create'
+        ];
 
+    })
 })
