cb12435628dcb8c9b11152c8fea67efb552f8cbd
azeez1776 <aboud3am81@gmail.com>
2022-02-15 09:18:05 +0300

    added the the array routes with objects consisting of path and method

--- src/resources/list/__test__/list.router.spec.js
+++ src/resources/list/__test__/list.router.spec.js
@@ -2,6 +2,13 @@ import router from '../list.router.js';
 
 describe("Testing the list router", () => {
     test("Testing the routes of the list", () => {
+    const routes = [
+        {path:'/', method:'get'},
+        {path:'/', method:'post'},
+        {path:'/:id', method:'get'},
+        {path:'/:id', method:'put'},
+        {path:'/:id', method:'delete'}
+    ];
 
     })
 })
